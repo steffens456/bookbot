c1c1d563a5be71b298c05daff66747dc8baed326
steffens456 <matt.steffens@doane.edu>
2025-02-22 18:09:58 -0600

    more updates

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ def main():
         from stats import frank_count
         num = frank_count(file_contents)
 
-        print(f"{num} words found in the document")
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print(f"Found {num} total words")
+        print("--------- Character Count -------")
+
 
         from stats import frank_strcount
-
         dict2 = frank_strcount(file_contents)
-        print(dict2)
+        #print(dict2)
+
+        
+        from stats import data_report
+        frank_str2 = data_report(dict2)
+        #print(frank_str2)
+
+        from stats import print_report
+        print_report(frank_str2)
+
 
 
 
--- stats.py
+++ stats.py
@@ -17,3 +17,38 @@ def frank_strcount(file_contents):
             dict1[c] = 1
 
     return dict1
+
+
+    #helper function
+def sort_on(dict2):
+    dog1 = list(dict2.values())[0]
+    return dog1
+
+def data_report(dict2):
+    
+
+    #create a list of dictionaries
+    sorted_counts = [{char: count} for char, count in dict2.items() if char.isalpha()]
+
+    #sort the list in place by descending order
+    sorted_counts.sort(reverse= True, key= sort_on)
+
+    return sorted_counts
+
+def print_report(frank_str2):
+    print(frank_str2[0])
+    print(frank_str2[3])
+
+    #it's a list of dictionaries
+
+    for i in range(0, len(frank_str2)):
+        dict3 = frank_str2[i]
+
+        str1 = str[dict3[0]]
+
+        num = float(dict3[1])
+        
+        frankenstring = f"{str1}: {num}"
+        print(frankenstring)
+
+    return
