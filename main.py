51fa8e3f5e9ca2ee3f0d4b362f8cd271c9abe737
steffens456 <matt.steffens@doane.edu>
2025-02-23 11:37:36 -0600

    adding sys module

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
+    #sys module
+    import sys
+
+    print('Usage: python3 main.py <path to books>', sys.argv)
+
+    book_path = sys.argv
+    file_contents = get_book_text(book_path)
+       
+    sys.exit(1)
+
+
+    
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+    #with open('books/frankenstein.txt') as f:
+     #   file_contents = f.read()
         #print(file_contents)
 
 
-        from stats import frank_count
-        num = frank_count(file_contents)
+    from stats import frank_count
+    num = frank_count(file_contents)
 
-        print("============ BOOKBOT ============")
-        print("Analyzing book found at books/frankenstein.txt...")
-        print(f"Found {num} total words")
-        print("--------- Character Count -------")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Found {num} total words")
+    print("--------- Character Count -------")
 
 
-        from stats import frank_strcount
-        dict2 = frank_strcount(file_contents)
-        #print(dict2)
+    from stats import frank_strcount
+    dict2 = frank_strcount(file_contents)
+    #print(dict2)
 
         
-        from stats import data_report
-        frank_str2 = data_report(dict2)
-        #print(frank_str2)
+    from stats import data_report
+    frank_str2 = data_report(dict2)
+    #print(frank_str2)
 
-        from stats import print_report
-        print_report(frank_str2)
+    from stats import print_report
+    print_report(frank_str2)
 
 
 
